32c5cc2108fb64a5544f792c0bb05e66fb400b96
Andrii Babushko <vt211_bas@student.ztu.edu.ua>
2024-05-05 19:20:45 +0300

    initialize docs config file to deploy on github pages

--- magidoc.mjs
+++ magidoc.mjs
@@ -1,9 +1,17 @@
+import path from 'path'
+
 export default {
   introspection: {
     type: 'sdl',
     paths: ['./src/schema.gql'],
   },
   website: {
+    staticAssets: path.join(__dirname, 'assets'),
+    options: {
+      // You need to specify the base path of your github pages root
+      // Example: `/magidoc`
+      siteRoot: '/<repo>',
+    },
     template: 'carbon-multi-page',
   },
 }
